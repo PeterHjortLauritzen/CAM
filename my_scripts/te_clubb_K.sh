c0eee7b6760cabbbb96ede65a968e9c5888dcbb3
Peter Hjort Lauritzen <pel@ucar.edu>
2021-12-27 15:56:17 -0700

    add CLUBB diags

---
+++ my_scripts/te_clubb_K.sh
@@ -0,0 +1,15 @@
+#!/bin/tcsh
+if ( "$#argv" != 2 ) then
+  echo "Wrong number of arguments specified:"
+  echo "  -arg 1 reference run"
+  echo "  -arg 2 scale surface flux run"
+  exit
+endif
+set n = 1
+set fileA = "$argv[$n]"
+set n = 2
+set fileB = "$argv[$n]"
+
+set dir='/glade/u/home/pel/src/cam_energy_analysis/my_scripts'
+ncl 'dir="'$PWD'"' 'fname_ref="'$fileA'"' 'fname_frc="'$fileB'"' $dir/te_clubb_K.ncl  
+
