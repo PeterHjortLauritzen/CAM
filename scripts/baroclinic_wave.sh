1b39d4af536e9c85dcdfb8d9e3e1a14a9058fe71
Peter Hjort Lauritzen <pel@ucar.edu>
2022-03-03 14:31:47 -0700

    script for running Jablonowski baroclinic wave

---
+++ scripts/baroclinic_wave.sh
@@ -0,0 +1,47 @@
+#!/bin/tcsh
+set res = ne30_ne30_mg17
+#
+# source code (assumed to be in /glade/u/home/$USER/src)
+#
+set src="cam-mars"
+#
+# number of test tracers
+#
+set stopoption="ndays"
+set steps="1"
+set cset="FADIAB"
+set pecount="225"
+set queue="regular"
+set PBS_ACCOUNT="P93300642"
+echo $PBS_ACCOUNT
+set scratch="/glade/scratch"
+
+set caze=${cset}_${res}
+/glade/u/home/$USER/src/$src/cime/scripts/create_newcase --case $scratch/$USER/$caze --compset $cset --res $res  --q $queue --walltime 00:10:00 --pecount $pecount  --project $PBS_ACCOUNT --run-unsupported
+
+cd $scratch/$USER/$caze
+./xmlchange STOP_OPTION=$stopoption,STOP_N=$steps
+./xmlchange DOUT_S=FALSE
+./xmlchange DEBUG=FALSE
+./xmlchange TIMER_LEVEL=10
+./xmlchange CAM_CONFIG_OPTS="-phys adiabatic -analytic_ic -cppdefs -Dplanet_mars -nlev 32"
+./case.setup
+
+echo "se_statefreq       = 1"                     >> user_nl_cam
+
+echo "interpolate_output = .true.,.true.,.true."    >> user_nl_cam
+echo "interpolate_nlat   = 192,192,192"             >> user_nl_cam
+echo "interpolate_nlon   = 288,288,288"             >> user_nl_cam
+echo "empty_htapes       = .true." >> user_nl_cam
+#held_suarez_1994,moist_baroclinic_wave_dcmip2016,dry_baroclinic_wave_dcmip2016,dry_baroclinic_wave_jw2006,us_standard_atmosphere
+echo "analytic_ic_type='dry_baroclinic_wave_jw2006'"  >> user_nl_cam
+echo "nhtfrq = -24" >> user_nl_cam
+echo "mfilt = 144" >> user_nl_cam
+echo "avgflag_pertape(1) = 'I'" >> user_nl_cam
+echo "fincl1 = 'PS','U','V','T','OMEGA'" >> user_nl_cam
+
+echo "se_nsplit = 6" >> user_nl_cam
+
+qcmd -- ./case.build
+./case.submit
+
